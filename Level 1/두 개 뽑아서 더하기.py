290cafb4bdc704847de543b2598bb0538ffc397a
DongChyeon <kimdonghyun90766@gmail.com>
2021-01-04 18:14:23 +0900

    Update 두 개 뽑아서 더하기.py

---
+++ Level 1/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    a = []
+    answer = []
+    
+    for i in range(0, len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+                
+    return sorted(list(set(answer)))
